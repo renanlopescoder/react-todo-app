a3bd31d8cde56fd17c7744b71e1e19bb37829dc4
Renan Lopes <renanlopescoder@gmail.com>
2017-07-18 17:11:02 -0300

    feature: Grid, If and iconButton Component
    feature: Grid Component to reduce bootstrap css names
    feature: iconButton Component to reuse code
    feature: If Component to show or hide a component

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,22 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 class Todo extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleAdd() {
+    
+  }
+
   render(){
     return (
       <div>
-        <PageHeader name='Tasks' small='New Task' />
-        <TodoForm></TodoForm>
+        <PageHeader name='Tasks' small='Registration' />
+        <TodoForm handleAdd={this.handleAdd} description={this.state.description}></TodoForm>
         <TodoList></TodoList>
       </div>
     )
